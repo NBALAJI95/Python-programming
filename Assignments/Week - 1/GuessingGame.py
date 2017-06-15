3843f9954dff1ad1f36ec5f7123fa44baadc2dac
Balaji Natarajan <balaji.natarajan.7@gmail.com>
2017-06-14 22:58:49 -0500

    Guessing game comments

--- Assignments/Week - 1/GuessingGame.py
+++ Assignments/Week - 1/GuessingGame.py
@@ -1,17 +1,23 @@
+# Guessing game by Balaji Natarajan
 import random
 
 def main():
+	# getting a single digit random value
     answer = random.randint(0,9)
     num_of_guess = 0
     print('Answer', answer)
+	# Repeat until correct
     while True:
         guess = int(input("Guess the digit: "))
+		# correct guess
         if guess == answer:
             print('Your answer is PERFECT!! Congratulations!!')
             num_of_guess += 1
             break
+		# low guess value
         elif guess < answer:
             print('Your answer is lower than required')
+		# high guess value
         elif guess > answer:
             print('Your answer is higher than required')
         num_of_guess += 1
