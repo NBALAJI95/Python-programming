8fd75c4ee529c5097d5fae250b84bc137d94c302
Balaji Natarajan <balaji.natarajan.7@gmail.com>
2017-06-14 23:04:08 -0500

    Rectangle program with comments

--- Assignments/Week - 1/Rectangle.py
+++ Assignments/Week - 1/Rectangle.py
@@ -1,12 +1,16 @@
 def perimeter_Rectangle(l, b):
+	# finding perimeter of a rectangle: 2 times length times breadth
     return 2 * (l + b)
 
 def area_Rectangle(l, b):
+	# finding area of rectangle: length times breadth
     return l * b
 
 def main():
+	# Get length and breadth as input
     length = int(input("Enter length:"))
     breadth = int(input("Enter breadth:"))
+	# print the final values
     print('Perimeter of the rectangle with length', length, 'and', 'breadth', breadth , 'is',perimeter_Rectangle(length, breadth))
     print('Area of the rectangle with length', length, 'and', 'breadth', breadth , 'is',area_Rectangle(length, breadth))
 
