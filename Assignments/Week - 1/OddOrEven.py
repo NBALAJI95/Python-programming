1919bd99b5e37fd863998738c45f6042fb673c80
Balaji Natarajan <balaji.natarajan.7@gmail.com>
2017-06-14 23:00:55 -0500

    Odd or even comments

--- Assignments/Week - 1/OddOrEven.py
+++ Assignments/Week - 1/OddOrEven.py
@@ -1,10 +1,13 @@
 def checkOddOrEven(n):
+	# if reminder is 0 after dividing by 2 it's even
     if n % 2 == 0:
         return "Number entered by user is even"
     else:
+	# else it is odd
         return "Number entered by user is odd"
 
 def main():
+	# Get a number as input
     num = int(input("Enter Number:"))
     print(checkOddOrEven(num))
 
