d1a12f3150b6e96b8910e779649f83119254b68f
Balaji Natarajan <balaji.natarajan.7@gmail.com>
2017-06-22 23:00:58 -0500

    Task-1 complete - Assignment-2 complete

---
+++ Assignments/Week - 2/Task - 1 - Letter.py
@@ -0,0 +1,19 @@
+def main():
+    op = ''
+    letter = [4, 2, 2, 2, 4]
+    for i in range(5):
+        for j in range(4):
+            if i == 0 or i == 4:
+                op += '* '
+            elif i == 1 or i== 3:
+                if j % 2 == 0:
+                    op += '* '
+                else:
+                    op += ' '
+            elif i == 2:
+                if j == 0 or j == 1:
+                    op += '* '
+        op += '\n'
+    print(op)
+if __name__ == '__main__':
+    main()
