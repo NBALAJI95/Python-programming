46439e57806cbccb2e4ac265d5e2d61c348e4e3e
Balaji Natarajan <balaji.natarajan.7@gmail.com>
2017-07-05 17:21:05 -0500

    Q3 completed -  inClass-04

---
+++ Assignments/Week - 4/Q3.py
@@ -0,0 +1,9 @@
+def main():
+    input_list = input('Input:')
+    input_list = input_list.split(',')
+    input_list.sort()
+    output_list = input_list
+    print(output_list)
+
+if __name__ == '__main__':
+    main()
